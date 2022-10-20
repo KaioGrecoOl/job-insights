fde2fe0b2da3ac0e594fcb22d14c1b56a2a6821e
Kaio G <kaiogreco91@gmail.com>
2022-10-20 17:46:41 -0300

    chore: iniciando o projeto

--- src/jobs.py
+++ src/jobs.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    """Reads a file from a given path and returns its content
 
     Parameters
     ----------
