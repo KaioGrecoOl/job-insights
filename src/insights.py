e0960213662a7a75bc9fea3225848ea2ebed8655
Kaio G <kaiogreco91@gmail.com>
2022-10-23 19:41:22 -0300

    feat: get unique job types
    desenvolve função que retorna uma lista de valores únicos presentes na coluna job_type

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,12 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    unique_jobs = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    for job in read(path):
+        unique_jobs.add(job["job_type"])
+    return unique_jobs
 
 
 def filter_by_job_type(jobs, job_type):
