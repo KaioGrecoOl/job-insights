04dca812a28615991f67c949d7f66387170f32c1
Kaio G <kaiogreco91@gmail.com>
2022-10-24 13:14:36 -0300

    feat: get_unique_industries
    retorna a lista de valores únicos presentes na coluna industry

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    unique_industries = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    for industry in read(path):
+        if industry["industry"] != '':
+            unique_industries.add(industry["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
