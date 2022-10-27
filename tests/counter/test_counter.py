40594299ee0fe1778b9f4201281ac62e6998faa3
Kaio G <kaiogreco91@gmail.com>
2022-10-27 18:43:59 -0300

    feat: test_count_ocurrences
    teste da contagem de palavras

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('src/jobs.csv', 'python') == count_ocurrences(
+        'src/jobs.csv', 'PYTHON')
